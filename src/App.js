7cebc0c047c0068d240d47fc0b48779712a86123
Jamgoori <lks2354@naver.com>
2023-01-19 18:39:31 +0900

    라우터연결

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import {Switch, Route, Link} from 'react-router-dom';
+import {Layout, Typography, Space} from "antd";
+import {Navbar} from './components'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+      <div className='main'>
+d
+      </div>
+      <div className='footer'>
+
+      </div>
+    </div>
   )
 }
 
