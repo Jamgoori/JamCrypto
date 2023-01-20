d8b38b39a45ca281bd2602d56eb0d57a7023ac07
Jamgoori <lks2354@naver.com>
2023-01-20 14:57:29 +0900

    라우터설정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
-import React from 'react'
-import {Switch, Route, Link} from 'react-router-dom';
-import {Layout, Typography, Space} from "antd";
-import {Navbar} from './components'
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+  HomePage,
+} from "./components";
+
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <div className='main'>
-d
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
       </div>
-      <div className='footer'>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<HomePage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
 
+        <div
+          className="footer"
+          level={5}
+          style={{ color: "white", textAlign: "center" }}
+        >
+          <Typography.Title>
+            Jam Crypto <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
